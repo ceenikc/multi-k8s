54263caade7bde6cebdbd9f81ef2cd8f6db498df
Nikhil Chadha <menikhilchadha@gmail.com>
2018-09-19 12:23:14 +0530

    added install kubectl instead of update

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-gcloud components update kubectl
+gcloud components install kubectl
 gcloud auth activate-service-account --key-file $multi-k8s
 gcloud config set project multi-k8s-215317
 gcloud config set compute/zone asia-south1-a
