bf21ec0732a7e4bae87607c46cf67bb6aa527d53
Nikhil Chadha <menikhilchadha@gmail.com>
2018-09-19 19:13:54 +0530

    test gcloud installed on jenkins server

--- test.sh
+++ test.sh
@@ -1,4 +1,3 @@
-# gcloud auth activate-service-account --key-file $GOOGLE_AUTH
-# gcloud config set project multi-k8s-215317
-# gcloud config set compute/zone asia-south1-a
-curl https://sdk.cloud.google.com | bash > /dev/null
+gcloud auth activate-service-account --key-file $GOOGLE_AUTH
+gcloud config set project multi-k8s-215317
+gcloud config set compute/zone asia-south1-a
