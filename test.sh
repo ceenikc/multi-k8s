30d0742ff4c163dc4a482e01ad3b21f11cf70b4f
Nikhil Chadha <menikhilchadha@gmail.com>
2018-09-19 19:08:25 +0530

    add travis gcloud install command

--- test.sh
+++ test.sh
@@ -1,8 +1,4 @@
 # gcloud auth activate-service-account --key-file $GOOGLE_AUTH
 # gcloud config set project multi-k8s-215317
 # gcloud config set compute/zone asia-south1-a
-export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update && sudo apt-get install google-cloud-sdk
-sudo apt-get install kubectl
+curl https://sdk.cloud.google.com | bash > /dev/null
