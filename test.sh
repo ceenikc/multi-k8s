0f85f3a47b4e0902feab3e012cdac6acd9e9d668
Nikhil Chadha <menikhilchadha@gmail.com>
2018-09-20 14:02:57 +0530

    check for SHA

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@ gcloud auth activate-service-account --key-file $GOOGLE_AUTH
 gcloud config set project multi-k8s-215317
 gcloud config set compute/zone asia-south1-a
 export SHA=$(git rev-parse HEAD)
-echo $SHA
+echo SHA=$SHA
